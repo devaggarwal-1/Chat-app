e98e05fe561ba2116003d51b84cc3b1fb3d1f80c
devaggarwal-1 <devaggarwal190@gmail.com>
2024-02-18 12:45:58 -0600

    Deplopyment changes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
 import generateTokenAndSetCookie from "../utils/generateToken.js";
-import { Await } from "react-router-dom";
+
 
 export const signup = async (req, res) => {
     try {
